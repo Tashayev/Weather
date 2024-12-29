1a3c7e74ccf9f0d87fe9318bee3466befe6e241a
Tashayev <113464233+Tashayev@users.noreply.github.com>
2024-12-29 23:24:19 +0500

    Update interfaces.tsx

--- src/interfaces.tsx
+++ src/interfaces.tsx
@@ -10,7 +10,7 @@ export type WeatherContextType = {
     weatherData: WeatherData | null;
     loading: boolean;
     error: string | null;
-    addCity: ()=>{};
+    addCity: (city: string)=>{};
     setPlaceName: (place: string) => void;
 
 };
